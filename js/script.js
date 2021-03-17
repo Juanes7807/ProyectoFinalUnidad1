06afa14c66d0883d40d57e59e55c60041a6fc2be
Juank Ibero <juank7807@hotmail.com>
2021-03-16 21:40:40 -0500

    ProyectoFinalUnidad1

---
+++ js/script.js
@@ -0,0 +1,44 @@
+alert("Bienvenidos a la vida de Sigmun Freud");
+confirm("¿Estas seguro de que quieres salir?");
+
+
+
+ function nombreautor () {
+    var nombreautor = prompt("¿Como se llama el autor del Psicoanalisis?")
+
+    if ( nombreautor =="Sigmund Freud") {
+        alert("El nombre es correcto");
+    } else {
+        alert("El nombre es incorrecto");
+    }
+ }
+
+ function nombreestudio () {
+    var nombreestudio = prompt("¿Que estupefaciente utiliza Freud para tratamientos?")
+
+    if ( nombreautor =="coca") {
+        alert("El nombre es correcto");
+    } else {
+        alert("El nombre es incorrecto");
+    }
+ }
+
+ function nombreobra () {
+    var nombreestudio = prompt("¿Cual es su obra mas importante?")
+
+    if ( nombreobra =="la interprtacion de los sueños") {
+        alert("El nombre es correcto");
+    } else {
+        alert("El nombre es incorrecto");
+    }
+ }
+
+ function nombremuerte () {
+    var nombreestudio = prompt("¿Cual fue la razon de su muerte?")
+
+    if ( nombremuerte =="cancer") {
+        alert("El nombre es correcto");
+    } else {
+        alert("El nombre es incorrecto");
+    }
+ }
